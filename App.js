df33af9570298587f3567acb32a9c48a07fcc1f6
Peizhi Liu <peizhiliu2023@u.northwestern.edu>
2021-03-22 13:01:39 -0400

    fetch data

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { SafeAreaView, StyleSheet, Text } from 'react-native';
 
 import CourseList from './components/CourseList'
@@ -30,6 +30,20 @@ const schedule = {
 };
 
 const App = () => {
+  const [schedule, setSchedule] = useState({title: '', courses: []});
+
+  const url = 'https://courses.cs.northwestern.edu/394/data/cs-courses.php';
+
+  useEffect(() => {
+    const fetchSchedule = async () => {
+      const response = await fetch(url);
+      if (!response.ok) throw response;
+      const json = await response.json();
+      setSchedule(json);
+    }
+    fetchSchedule();
+  }, [])
+
   return (
     <SafeAreaView style={styles.container}>
       <Banner title={schedule.title} />
@@ -39,7 +53,7 @@ const App = () => {
 }
 
 const Banner = ({title}) => (
-  <Text style={styles.bannerStyle}>{title}</Text>
+  <Text style={styles.bannerStyle}>{title || '[loading...]'}</Text>
 );
 
 const styles = StyleSheet.create({
