1026596938defa2c32bbf725f486de616ef54445
Peizhi Liu <peizhiliu2023@u.northwestern.edu>
2021-03-25 15:32:28 -0400

    authenticate users

--- screens/ScheduleScreen.js
+++ screens/ScheduleScreen.js
@@ -3,7 +3,6 @@ import { SafeAreaView, StyleSheet, Text } from 'react-native';
 
 import CourseList from '../components/CourseList';
 import UserContext from '../UserContext';
-import CourseEditScreen from './CourseEditScreen';
 import {firebase} from '../utils/firebase';
 
 const fixCourses = json => ({
